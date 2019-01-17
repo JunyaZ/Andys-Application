25752cd1bccd3311d49046816b5626498df16153
Junya Zhao <31264540+JunyaZ@users.noreply.github.com>
2019-01-17 10:51:04 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 20 16:59:07 2018
+
+@author: Junya
+"""''
+from tkinter import *
+from tkinter.messagebox import *
+from Gui import LoginPage
+from Gui import DatabaseInterface
+
+
+root = Tk()  
+root.title('Manager Login')  
+width = 500  
+height =700
+screenwidth = root.winfo_screenwidth()    
+screenheight = root.winfo_screenheight()   
+alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth-width)/2, (screenheight-height)/2)  
+root.geometry(alignstr)    # align center
+
+"""Start program."""
+page1 = LoginPage()
+root.mainloop()
+print(" Closed the connetion to the DB")
+        
---
+++ run_install.bat
@@ -0,0 +1,5 @@
+THI@echo off
+
+Powershell.exe -executionpolicy remotesigned -File  C:\Users\Folder\install.ps1
+PAUSE
+
